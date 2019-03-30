e072918d2dc6b4d8e623447029487fd67d64afdc
Alexandra Bozzini <alexandrabozzini@zipcodersmbp74.lan>
2019-03-30 17:20:05 -0400

    Initial commit

---
+++ src/main/java/com/alexbozzini/coffeenote/CoffeenoteApplication.java
@@ -0,0 +1,13 @@
+package com.alexbozzini.coffeenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeenoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeenoteApplication.class, args);
+    }
+
+}
