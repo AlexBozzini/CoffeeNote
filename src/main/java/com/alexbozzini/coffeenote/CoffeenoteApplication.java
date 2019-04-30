764198cc5f629564b70cf7ba2a78ea10f7efbf9b
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-30 18:05:10 -0400

    Fixed time zone

--- src/main/java/com/alexbozzini/coffeenote/CoffeenoteApplication.java
+++ src/main/java/com/alexbozzini/coffeenote/CoffeenoteApplication.java
@@ -3,8 +3,16 @@ package com.alexbozzini.coffeenote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CoffeenoteApplication {
+
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CoffeenoteApplication.class, args);
